70456815c32f6b5597e731590127d8e9743cc907
Vincent Ho <doiphongba@gmail.com>
2017-12-07 15:35:07 -0500

    restore back to normal state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, em"ltaolao
+    render html: "hello, em"
   end
 end
